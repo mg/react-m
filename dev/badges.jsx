8f6dfd18136cea3b996b9ad16a66110dc2afba5b
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2015-12-25 12:45:45 +0000

    Badge implementation

--- dev/badges.jsx
+++ dev/badges.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Badge } from '../src'
-import { LeftPackedRow, Space } from './layouts.jsx'
+import { SpreadRow } from './layouts.jsx'
 
 export default class Badges extends React.Component {
   render() {
     return (
       <div>
         <h3>Badges</h3>
+        <SpreadRow>
+          <Badge value='1'>Text</Badge>
+          <Badge value='2' color='rgb(76,175,80)'>Text</Badge>
+          <Badge value='alarm' icon>Text</Badge>
+          <Badge value='3' align='left'>Text</Badge>
+        </SpreadRow>
+
       </div>
     )
   }
--- src/badge.jsx
+++ src/badge.jsx
@@ -3,20 +3,37 @@ import Radium from 'radium'
 
 export class Badge extends React.Component {
   static propTypes= {
-
+    value: React.PropTypes.string.isRequired,
+    icon: React.PropTypes.bool,
+    color: React.PropTypes.string,
+    align: React.PropTypes.oneOf(['left', 'right'])
   }
 
   static defaultProps= {
-
+    icon: false,
+    align: 'right',
+    color: '#ff4081',
   }
 
   render() {
-    const {}= this.props
+    const { value, icon, color, align, children }= this.props
+
+    let styleBadge= {...styles.badge, backgroundColor: color}
+    if(icon) {
+      styleBadge= {...styleBadge, ...styles.icon}
+    }
+    if(align === 'left') {
+      delete styleBadge.right
+      styleBadge= {...styleBadge, left: -24}      
+    }
 
     return (
-      <div>
-
-      </div>
+      <span style={styles.container}>
+        {children}
+        <div style={styleBadge}>
+          {value}
+        </div>
+      </span>
     )
   }
 }
@@ -24,5 +41,33 @@ export class Badge extends React.Component {
 export default Radium(Badge)
 
 const styles= {
-  
+  container: {
+    position: 'relative',
+  },
+
+  badge: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -12,
+    right: -24,
+
+    fontWeight: 600,
+    fontSize: 12,
+    width: 22,
+    height: 22,
+    borderRadius: '50%',
+    color: '#fff',
+  },
+
+  icon: {
+    fontFamily: 'Material Icons',
+    fontStyle: 'normal',
+    textTransform: 'none',
+    fontFeatureSettings: '\'liga\'',
+    WebkitFontFeatureSettings: '\'liga\'',
+    WebkitFontSmoothing: 'antialiased',
+},
+
 }
