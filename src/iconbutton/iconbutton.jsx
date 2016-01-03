f2dde81c40077a62f099b9d9a72f75523310217f
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-03 22:00:05 +0000

    IconButton: refactor styles

--- src/iconbutton/iconbutton.jsx
+++ src/iconbutton/iconbutton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Radium from 'radium'
 import Color from 'color'
 import Ripple from '../ripple.jsx'
+import styles from './styles.js'
 
 export class IconButton extends React.Component {
   static propTypes= {
@@ -76,57 +77,3 @@ export class IconButton extends React.Component {
 }
 
 export default Radium(IconButton)
-
-import VendorPrefix from 'react-vendor-prefixes'
-let styles = VendorPrefix.prefix({
-  button: {
-    borderRadius: '50%',
-    fontSize: 24,
-    height: 32,
-    minWidth: 32,
-    outline: 'none',
-    backgroundColor: 'initial',
-    border: 'none',
-    willChange: 'box-shadow,transform',
-    transition: 'box-shadow .2s cubic-bezier(.4,0,1,1),background-color .2s cubic-bezier(.4,0,.2,1),color .2s cubic-bezier(.4,0,.2,1)',
-    outline: 'none',
-    cursor: 'pointer',
-
-    ':hover': {
-      backgroundColor: 'rgba(158,158,158,.2)',
-    },
-
-    ':active': {
-      backgroundColor: 'rgba(158,158,158,.4)',
-    },
-  },
-
-  icon: {
-    verticalAlign: 'middle',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-12px,-12px)',
-    lineHeight: '24px',
-    width: 24,
-    fontFamily: 'Material Icons',
-    fontStyle: 'normal',
-    textTransform: 'none',
-    fontFeatureSettings: '\'liga\'',
-    WebkitFontFeatureSettings: '\'liga\'',
-    WebkitFontSmoothing: 'antialiased',
-  },
-
-  disabled: {
-    color: 'rgba(0,0,0,.26)',
-    cursor: 'default',
-  },
-
-  ripple: {
-    top: 0,
-    left: 0,
-    height: '100%',
-    width: '100%',
-    borderRadius: '50%',
-  },
-})
---
+++ src/iconbutton/styles.js
@@ -0,0 +1,52 @@
+export default {
+  button: {
+    borderRadius: '50%',
+    fontSize: 24,
+    height: 32,
+    minWidth: 32,
+    outline: 'none',
+    backgroundColor: 'initial',
+    border: 'none',
+    willChange: 'box-shadow,transform',
+    transition: 'box-shadow .2s cubic-bezier(.4,0,1,1),background-color .2s cubic-bezier(.4,0,.2,1),color .2s cubic-bezier(.4,0,.2,1)',
+    outline: 'none',
+    cursor: 'pointer',
+
+    ':hover': {
+      backgroundColor: 'rgba(158,158,158,.2)',
+    },
+
+    ':active': {
+      backgroundColor: 'rgba(158,158,158,.4)',
+    },
+  },
+
+  icon: {
+    verticalAlign: 'middle',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-12px,-12px)',
+    lineHeight: '24px',
+    width: 24,
+    fontFamily: 'Material Icons',
+    fontStyle: 'normal',
+    textTransform: 'none',
+    fontFeatureSettings: '\'liga\'',
+    WebkitFontFeatureSettings: '\'liga\'',
+    WebkitFontSmoothing: 'antialiased',
+  },
+
+  disabled: {
+    color: 'rgba(0,0,0,.26)',
+    cursor: 'default',
+  },
+
+  ripple: {
+    top: 0,
+    left: 0,
+    height: '100%',
+    width: '100%',
+    borderRadius: '50%',
+  },  
+}
