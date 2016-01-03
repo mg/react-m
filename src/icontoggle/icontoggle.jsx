dcabf31f1d988a0df75b60f12f69717b47c03ef2
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-03 22:00:13 +0000

    IconToggle: refactor styles

--- src/icontoggle/icontoggle.jsx
+++ src/icontoggle/icontoggle.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Radium from 'radium'
 import { AutoId } from 'react-autoid'
 import Ripple from '../ripple.jsx'
+import styles from './styles.js'
 
 export class IconToggle extends React.Component {
   static propTypes= {
@@ -97,79 +98,3 @@ export class IconToggle extends React.Component {
 }
 
 export default AutoId(Radium(IconToggle))
-
-const styles = {
-  container: {
-    position: 'relative',
-    width: 32,
-    height: 32,
-    cursor: 'pointer',
-  },
-
-  link: {
-    color: '#616161',
-    display: 'block',
-  },
-
-  linkOn: {
-    color: '#3f51b5',
-  },
-
-  icon: {
-    lineHeight: '32px',
-    fontSize: 24,
-    verticalAlign: 'middle',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-12px,-12px)',
-    width: 24,
-    fontFamily: 'Material Icons',
-    fontStyle: 'normal',
-    textTransform: 'none',
-    fontFeatureSettings: '\'liga\'',
-    WebkitFontFeatureSettings: '\'liga\'',
-    WebkitFontSmoothing: 'antialiased',
-  },
-
-  input: {
-    appearance: 'none',
-    position: 'absolute',
-    width: 0,
-    height: 0,
-    margin: 0,
-    padding: 0,
-    opacity: 0,
-    border: 'none',
-  },
-
-  focus: {
-    position: 'absolute',
-    top: 10,
-    left: 8,
-    display: 'inline-block',
-    boxSizing: 'border-box',
-    width: 16,
-    height: 16,
-    borderRadius: '50%',
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
-    transitionDuration: '.28s',
-    transitionTimingFunction: 'cubic-bezier(.4,0,.2,1)',
-    transitionProperty: 'background-color box-shadow',
-  },
-
-  focusOn: {
-    backgroundColor: 'rgba(63,81,181,.26)',
-    boxShadow: '0 0 0 8px rgba(63,81,181,.26)',
-  },
-
-  ripple: {
-    top: 0,
-    left: -2,
-    width: 36,
-    height: 36,
-    borderRadius: '50%',
-    WebkitMaskImage: '-webkit-radial-gradient(circle,#fff,#000)',
-  },
-}
---
+++ src/icontoggle/styles.js
@@ -0,0 +1,75 @@
+export default {
+  container: {
+    position: 'relative',
+    width: 32,
+    height: 32,
+    cursor: 'pointer',
+  },
+
+  link: {
+    color: '#616161',
+    display: 'block',
+  },
+
+  linkOn: {
+    color: '#3f51b5',
+  },
+
+  icon: {
+    lineHeight: '32px',
+    fontSize: 24,
+    verticalAlign: 'middle',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-12px,-12px)',
+    width: 24,
+    fontFamily: 'Material Icons',
+    fontStyle: 'normal',
+    textTransform: 'none',
+    fontFeatureSettings: '\'liga\'',
+    WebkitFontFeatureSettings: '\'liga\'',
+    WebkitFontSmoothing: 'antialiased',
+  },
+
+  input: {
+    appearance: 'none',
+    position: 'absolute',
+    width: 0,
+    height: 0,
+    margin: 0,
+    padding: 0,
+    opacity: 0,
+    border: 'none',
+  },
+
+  focus: {
+    position: 'absolute',
+    top: 10,
+    left: 8,
+    display: 'inline-block',
+    boxSizing: 'border-box',
+    width: 16,
+    height: 16,
+    borderRadius: '50%',
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    transitionDuration: '.28s',
+    transitionTimingFunction: 'cubic-bezier(.4,0,.2,1)',
+    transitionProperty: 'background-color box-shadow',
+  },
+
+  focusOn: {
+    backgroundColor: 'rgba(63,81,181,.26)',
+    boxShadow: '0 0 0 8px rgba(63,81,181,.26)',
+  },
+
+  ripple: {
+    top: 0,
+    left: -2,
+    width: 36,
+    height: 36,
+    borderRadius: '50%',
+    WebkitMaskImage: '-webkit-radial-gradient(circle,#fff,#000)',
+  },  
+}
