e181a5c34a46c0ae53bbcf603d66c9a6a551060a
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-03 22:01:58 +0000

    Tooltip: refactor styles

---
+++ src/tooltip/styles.js
@@ -0,0 +1,32 @@
+export default {
+  container: {
+    position: 'relative',
+  },
+
+  tooltip: {
+    fontFamily: 'Roboto,Helvetica,Arial,sans-serif',
+    transformOrigin: 'top center',
+    willChange: 'transform',
+    zIndex: 999,
+    background: 'rgba(97,97,97,.9)',
+    borderRadius: 2,
+    color: '#fff',
+    display: 'inline-block',
+    fontSize: 10,
+    fontWeight: 500,
+    lineHeight: '14px',
+    maxWidth: 170,
+    textAlign: 'center',
+  },
+
+  tooltipHide: {
+    position: 'fixed',
+    top: -500,
+  },
+
+  tooltipCalced: {
+    position: 'absolute',
+    padding: 8,
+    transform: 'scale(0)',
+  }  
+}
--- src/tooltip/tooltip.jsx
+++ src/tooltip/tooltip.jsx
@@ -1,6 +1,7 @@
 import './styles.css'
 import React from 'react'
 import Radium from 'radium'
+import styles from './styles.js'
 
 export class Tooltip extends React.Component {
   static propTypes= {
@@ -85,36 +86,3 @@ export class Tooltip extends React.Component {
 }
 
 export default Radium(Tooltip)
-
-const styles= {
-  container: {
-    position: 'relative',
-  },
-
-  tooltip: {
-    fontFamily: 'Roboto,Helvetica,Arial,sans-serif',
-    transformOrigin: 'top center',
-    willChange: 'transform',
-    zIndex: 999,
-    background: 'rgba(97,97,97,.9)',
-    borderRadius: 2,
-    color: '#fff',
-    display: 'inline-block',
-    fontSize: 10,
-    fontWeight: 500,
-    lineHeight: '14px',
-    maxWidth: 170,
-    textAlign: 'center',
-  },
-
-  tooltipHide: {
-    position: 'fixed',
-    top: -500,
-  },
-
-  tooltipCalced: {
-    position: 'absolute',
-    padding: 8,
-    transform: 'scale(0)',
-  }
-}
