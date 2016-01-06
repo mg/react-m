3eb37cf39282fe4fcf69d8e56284cad8551f9596
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-06 00:26:58 +0000

    dev: add space to input

--- dev/input.jsx
+++ dev/input.jsx
@@ -9,13 +9,12 @@ export default class InputPage extends React.Component {
         <h3>Inputs</h3>
         <LeftPackedRow>
           <Input label='Text...' value={this.state.input1} onChange={input1 => this.setState({input1})}/>
+          <Space/>
           <Input label='Text...' value={this.state.input2} onChange={input2 => this.setState({input2})} error='There is an error.'/>
         </LeftPackedRow>
       </div>
     )
   }
 
-  state= {
-    input1: '',
-  }
+  state= {}
 }
