ad800430931b1d3e5cdaa2a5a71ea7b0d9588dd8
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-10 18:04:27 +0000

    Dev: fix layout on checkbox page

--- dev/checkbox.jsx
+++ dev/checkbox.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Checkbox } from '../src'
-import { LeftPackedRow, Space } from './layouts.jsx'
 
 const noop= () => {}
 
@@ -9,15 +8,14 @@ export default class CheckboxPage extends React.Component {
     return (
       <div>
         <h3>Checkbox</h3>
-        <LeftPackedRow>
-          <Checkbox value={this.state.checked1} onChange={checked1 => this.setState({checked1})}>Checkbox</Checkbox>
-          <Space/>
-          <Checkbox ripple value={this.state.checked2} onChange={checked2 => this.setState({checked2})}>With ripple</Checkbox>
-          <Space/>
-          <Checkbox disabled value={true} onChange={noop}>Disabled</Checkbox>
-          <Space/>
-          <Checkbox disabled value={false} onChange={noop}>Disabled</Checkbox>
-        </LeftPackedRow>
+        <Checkbox value={this.state.checked1} onChange={checked1 => this.setState({checked1})}>Label</Checkbox>
+
+        <h3>With ripple</h3>
+        <Checkbox ripple value={this.state.checked2} onChange={checked2 => this.setState({checked2})}>Label</Checkbox>
+
+        <h3>Disabled</h3>
+        <Checkbox disabled value={true} onChange={noop}>Label</Checkbox>
+        <Checkbox disabled value={false} onChange={noop}>Label</Checkbox>
       </div>
     )
   }
