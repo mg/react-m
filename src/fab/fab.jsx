cf219b5cc2e9ad861f7f216ffe7890b8778b3c7b
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-13 23:15:22 +0000

    Fab: use Icon, closes #43

--- src/fab/fab.jsx
+++ src/fab/fab.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Radium from 'radium'
 import Color from 'color'
 import Ripple from '../ripple'
+import Icon from '../icon'
 import styles from './styles.js'
 
 export class Fab extends React.Component {
@@ -24,11 +25,12 @@ export class Fab extends React.Component {
     let styleBase= {...styles.buttonBase}
     styleButton.color= color
     let rippleColor= color
+    let iconColor= color
     if(color !== '#000' && !disabled) {
       let c= Color(color)
       c.alpha(0.9).lighten(0.1)
       if(ripple !== true) styleButton[':focus'].backgroundColor= c.rgbString()
-      styleBase.color= 'white'
+      iconColor= 'white'
       styleBase.backgroundColor= color
       let rc= Color(color).alpha(0.7).lighten(0.7)
       rippleColor= rc.rgbString()
@@ -43,7 +45,7 @@ export class Fab extends React.Component {
           style={[styleButton, styleBase, styles.disabled]}
           disabled
           >
-          <i style={styles.icon}>{children}</i>
+          <Icon color='rgba(0,0,0,.26)'>{children}</Icon>
         </button>
       )
     }
@@ -65,7 +67,7 @@ export class Fab extends React.Component {
         onTouchEnd={::this.onEnd}
         ref={e => this.button= e}
         >
-        <i style={styles.icon}>{children}</i>
+        <Icon color={iconColor}>{children}</Icon>
         {markupRipple}
       </button>
     )
--- src/fab/styles.js
+++ src/fab/styles.js
@@ -1,8 +1,5 @@
 export default {
   button: {
-    //color: '#000',
-
-
     fontFamily: 'Roboto,Helvetica,Arial,sans-serif',
     fontSize: 24,
     fontWeight: 500,
@@ -49,24 +46,7 @@ export default {
     boxShadow: '0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12)',
   },
 
-  icon: {
-    verticalAlign: 'middle',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-12px,-12px)',
-    lineHeight: '24px',
-    width: 24,
-    fontFamily: 'Material Icons',
-    fontStyle: 'normal',
-    textTransform: 'none',
-    fontFeatureSettings: '\'liga\'',
-    WebkitFontFeatureSettings: '\'liga\'',
-    WebkitFontSmoothing: 'antialiased',
-  },
-
   disabled: {
-    color: 'rgba(0,0,0,.26)',
     cursor: 'default',
   },
 
