bc9678e045b1a68009e433b9c102246b8e1b605a
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-16 19:38:11 +0000

    Icon: support custom styles

--- src/icon/icon.jsx
+++ src/icon/icon.jsx
@@ -6,17 +6,19 @@ export class Icon extends React.Component {
   static propTypes= {
     color: React.PropTypes.string,
     size: React.PropTypes.number,
+    style: React.PropTypes.object,
   }
 
   static defaultProps= {
     color: '#757575',
     size: 24,
+    style: {},
   }
 
   render() {
-    const { color, size, children}= this.props
+    const { color, size, style, children }= this.props
 
-    let styleIcon= {...styles.icon, color: color, fontSize: size }
+    let styleIcon= {...styles.icon, color: color, fontSize: size, ...style }
 
     return (
       <div style={styleIcon}>
